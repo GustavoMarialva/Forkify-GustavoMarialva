6dc7922c4e20c1bc3003b8d3e9aba6bd788c96f3
Gustavo Marialva <gmarialva@gmail.com>
2023-08-23 14:29:45 -0300

    deploy

---
+++ README.md
@@ -0,0 +1,5 @@
+# forkify Project
+
+Recipe application with custom recipe uploads.
+
+npx browserslist@latest --update-db
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '365e7877-e675-4a3a-a99d-673ae992fa87';
+export const MODAL_CLOSE_SEC = 2.5;
